8fe8b9741fbe6b746698680fbee5a1d91b02ce0d
noion <n3t1m3n@gmail.com>
2024-06-02 18:52:35 +0300

    test statement

--- src/test/java/ru/noion/jsalmon/InterpreterTest.java
+++ src/test/java/ru/noion/jsalmon/InterpreterTest.java
@@ -2,6 +2,8 @@ package ru.noion.jsalmon;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -30,5 +32,37 @@ class InterpreterTest {
 
         // then
         Assertions.assertTrue(errorContent.toString().contains("Division by zero"));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "print 1 + 2;, 3",
+            "print 1 + 2 * 3;, 7",
+            "print 1 + \"a\";, 1a",
+            "print true;, true",
+            "print \"a\" + \"b\";, ab",
+    })
+    void interpret_print(String source, String expected) {
+        // given
+        var scanner = new Scanner(source, "");
+        var tokens = scanner.scanTokens();
+        var parser = new Parser(tokens);
+        var statements = parser.parse();
+        var interpreter = new Interpreter();
+
+        // Prepare to capture output stream
+        var outContent = new ByteArrayOutputStream();
+        var originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+
+        // when
+        interpreter.interpret(statements);
+
+        // Reset error stream back to original
+        System.setErr(originalOut);
+
+        // then
+        var result = outContent.toString().replace(System.lineSeparator(), "");
+        Assertions.assertEquals(expected, result);
     }
 }
